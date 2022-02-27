f988dd0ffbbb772b2b9691e4eaa773a153d431bc
AnnaSluzhevskaya <59249261+ielana2@users.noreply.github.com>
2022-02-27 05:10:31 +0300

    done task
    all code is in main file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,67 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+abstract class Video(val title: String)
 
-    // Try adding program arguments at Run/Debug configuration
-    println("Program arguments: ${args.joinToString()}")
+enum class MovieGenre {
+   ACTION, 
+   COMEDY,
+   DRAMA,
+   FANTASY,
+   HORROR,
+   MYSTERY,
+   ROMANCE,
+   THRILLER
+}
+
+class Movie(title: String, val genre: MovieGenre, val releaseYear: Int) : Video(title)
+
+class YoutubeVideo(title: String, val link: String) : Video(title)
+
+class TvShowEpisode(title: String, val season: Int, val series: Int) : Video(title)
+
+class PlayList() {
+   val videos: MutableList<Video> = mutableListOf()
+
+   fun addVideo(video: Video) {
+       videos.add(video)
+   }
+
+   fun playNext() {
+       println("Now playing: " + videos[0].title)
+       videos.removeAt(0)
+       println("")
+   }
+
+   fun getAllVideos() {
+       println("All videos in playlist:")
+       for (video in videos){
+           println(video.title)
+       }
+
+       println("")
+   }
+}
+
+fun main() {
+    val firstMovie: Movie = Movie("Lock, Stock and Two Smoking Barrels", MovieGenre.COMEDY, 1998)
+    val secondMovie: Movie = Movie("Fight Club", MovieGenre.THRILLER, 1999)
+    val firstYoutubeVideo: YoutubeVideo = YoutubeVideo("PSY - GANGNAM STYLE(강남스타일) M/V", "https://www.youtube.com/watch?v=9bZkp7q19f0")
+    val secondYoutubeVideo: YoutubeVideo = YoutubeVideo("Pink Panther Prances Through Cornfields | 35 Minute Compilation | The Pink Panther Show", "https://www.youtube.com/watch?v=3ZnC4DTshc4")
+    val firstTvShowEpisode: TvShowEpisode = TvShowEpisode("eps2.1_k3rnel-pan1c.ksd", 2, 3)
+    val secondTvShowEpisode: TvShowEpisode = TvShowEpisode("Family Tree", 1, 5)
+    val playList: PlayList = PlayList()
+    playList.addVideo(firstMovie)
+    playList.addVideo(firstYoutubeVideo)
+    playList.addVideo(firstTvShowEpisode)
+
+    playList.getAllVideos()
+    playList.playNext()
+
+    playList.addVideo(secondMovie)
+    playList.addVideo(secondYoutubeVideo)
+
+    playList.getAllVideos()
+    playList.playNext()
+
+    playList.addVideo(secondTvShowEpisode)
+    playList.getAllVideos()
+    playList.playNext()
 }
